e11c3129b7c59d809174efc72912002905826646
Karol Działowski <karlososhd@gmail.com>
2020-02-22 20:42:02 +0100

    Updates

---
+++ precache-manifest.8a9215b7d84be4db48d3057be595ea8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfb39dd6811dd570be79d3cc0919611",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "0f922e59bb950c3abef2",
+    "url": "/tic-tac-toe-react/static/css/main.752a6163.chunk.css"
+  },
+  {
+    "revision": "d798eb59a56fbff3d2cb",
+    "url": "/tic-tac-toe-react/static/js/2.59f09b58.chunk.js"
+  },
+  {
+    "revision": "eb16858e21cecd78a7f3de272899fffc",
+    "url": "/tic-tac-toe-react/static/js/2.59f09b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f922e59bb950c3abef2",
+    "url": "/tic-tac-toe-react/static/js/main.f94ebcce.chunk.js"
+  },
+  {
+    "revision": "6ed7bf54699633c88008",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.4f8da580.js"
+  }
+]);
